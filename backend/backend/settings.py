3d3c5b0326d24280b86163729418f325a9306d58
AKalamaghin <arteom.kalamaghin@isa.utm.md>
2022-12-23 18:51:37 +0200

    (backend-branch) CORS incident

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -53,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware'
 ]
 
 ROOT_URLCONF = 'backend.urls'
