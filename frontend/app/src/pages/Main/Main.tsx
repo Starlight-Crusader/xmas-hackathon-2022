1afae032acf1cc85f3e11bfc5a9fd48df0ff00e7
micha <michael.echim@gmail.com>
2022-12-23 15:53:00 +0200

    updated ui

--- frontend/app/src/pages/Main/Main.tsx
+++ frontend/app/src/pages/Main/Main.tsx
@@ -1,20 +1,22 @@
 import './Main.css'
-import {Col, Row} from 'antd'
-import { Grid } from '@material-ui/core'
+import {Col, Row, Space} from 'antd'
 
 export function Main() {
     return (
         <div className = "Main">
             <Row> 
-                <Col span = {16}><div className = "OutWin" ></div></Col>
-                <Col span = {2}></Col>
-                <Col span = {6}><div className = "FotoPalce"></div></Col>
+                <Col span = {16} ><div className = "OutWin" ></div></Col>
+                <Col span={2}></Col>
+                <Col span = {6} ><div className = "FotoPlace"></div></Col>
             </Row>
-            {/* <Row>
-                <form>
-                    <input className = "FormInput" placeholder = "Enter your commands..."/>
-                </form>
-            </Row> */}
+            <Row>
+                <Col span={16}>
+                    <form>
+                        <input className = "FormInput" placeholder = "Enter your commands..."/>
+                    </form>
+                </Col>
+                
+            </Row>
         </div>
     )
 }
