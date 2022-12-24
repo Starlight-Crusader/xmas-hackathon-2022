3759cc380e07e79fc81347ccbb5e8dddae0fe021
Sevastian Bajenov <sevabaj@gmail.com>
2022-12-24 10:01:52 +0200

    Fix buggggg.

--- backend/parseapp/views.py
+++ backend/parseapp/views.py
@@ -50,7 +50,7 @@ def parse_command(request):
                     return response.Response('Invalid argument', status=status.HTTP_400_BAD_REQUEST)
                 return response.Response('api/students/delete?name=' + student_name, status=status.HTTP_200_OK)
             elif tokens[2] == '-killist':
-                return response.Response('api/students/list/', status=status.HTTP_200_OK)
+                return response.Response('api/students/list', status=status.HTTP_200_OK)
 
         else:
             return response.Response('Invalid argument', status=status.HTTP_400_BAD_REQUEST)
